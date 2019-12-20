7ba04388d4946b42ab6a412f08e0fd418424e75b
Shaun Becker <smbecker@gmail.com>
2019-12-20 10:43:08 -0500

    Updated rider

--- desktop/rider.sh
+++ desktop/rider.sh
@@ -2,8 +2,8 @@
 
 mkdir -p $HOME/Tools/bin
 
-wget -c https://download.jetbrains.com/rider/JetBrains.Rider-2019.3.tar.gz -O - | tar -xz -C $HOME/Tools/
-mv $HOME/Tools/JetBrains\ Rider-2019.3/ $HOME/Tools/Rider/
+wget -c https://download.jetbrains.com/rider/JetBrains.Rider-2019.3.1.tar.gz -O - | tar -xz -C $HOME/Tools/
+mv $HOME/Tools/JetBrains\ Rider-2019.3.1/ $HOME/Tools/Rider/
 chmod -R 777 $HOME/Tools/Rider/
 ln -sf $HOME/Tools/Rider/bin/rider.sh $HOME/Tools/bin/rider
 
@@ -13,7 +13,7 @@ sudo sysctl -p --system
 
 sudo apt install -y libarchive-tools jq
 
-mkdir -p $HOME/.Rider2019.3
+mkdir -p $HOME/.Rider2019.3/config/plugins
 
 # Install Cyclomatic Complexity plugin
 wget -qO- https://plugins.jetbrains.com/files/$(curl https://plugins.jetbrains.com/api/plugins/10395/updates | jq -r '.[0].file') | bsdtar -xvf- -C $HOME/.Rider2019.3/config/plugins
