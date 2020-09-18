a82f67933700081744105fd210bf8735a1d337ad
Shaun Becker <smbecker@gmail.com>
2020-09-18 09:16:05 -0400

    Update mitm.sh

--- install/mitm.sh
+++ install/mitm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+mkdir -p $HOME/Tools/bin
 wget -c https://snapshots.mitmproxy.org/5.1.1/mitmproxy-5.1.1-linux.tar.gz -O - | tar -xz -C $HOME/Tools/bin
 
 if ! [ -d "$HOME/.mitmproxy" ] ; then
   echo "Launching mitmweb in order to generate certificates"
-  mitmweb --listen-port 8888 &
+  $HOME/Tools/bin/mitmweb --listen-port 8888 &
 
   echo "Waiting mitmweb to launch on 8888..."
   while ! nc -z localhost 8888; do
