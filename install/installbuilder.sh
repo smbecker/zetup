b40f28f2ea3bad56f721898e4aa03e8a9159fc48
Shaun Becker <smbecker@gmail.com>
2022-02-17 17:36:27 -0500

    Remove installbuilder

--- install/installbuilder.sh
+++
@@ -1,8 +0,0 @@
-wget -q https://installbuilder.com/installbuilder-enterprise-20.4.0-linux-x64-installer.run -O installbuilder.run
-chmod +x installbuilder.run
-sudo ./installbuilder.run --unattendedmodeui minimal --mode unattended --installer-language en
-rm installbuilder.run
-
-if [ -f "$HOME/secrets/installbuilder/license.xml" ]; then
-  cp "$HOME/secrets/installbuilder/license.xml" /opt/installbuilder-20.4.0/
-fi
