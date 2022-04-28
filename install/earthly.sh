33d03636b9230547e8065dc8dd081d84a52ee9a1
Shaun Becker <smbecker@gmail.com>
2022-04-28 11:26:02 -0400

    Add earthly install script

---
+++ install/earthly.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo /bin/sh -c 'wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap --with-autocomplete'
