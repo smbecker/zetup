6d7938ab9235e9a4f4ca8df79adcecf7045d3c1f
Shaun Becker <smbecker@gmail.com>
2022-04-29 16:27:38 -0400

    Fixed kind version

--- install/kubernetes.sh
+++ install/kubernetes.sh
@@ -8,7 +8,7 @@ sudo apt install -y kubectl
 
 # Install kind
 mkdir -p $HOME/Tools/bin
-curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.12.1/kind-$(uname)-amd64"
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.12.0/kind-$(uname)-amd64"
 chmod +x ./kind
 mv ./kind $HOME/Tools/bin/
 
