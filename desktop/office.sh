fd5599491b0e8d24ca2dc3d5c699fe85fcb7d207
Shaun Becker <smbecker@gmail.com>
2024-11-25 09:41:25 -0500

    Updates to latest

--- desktop/office.sh
+++ desktop/office.sh
@@ -5,12 +5,10 @@ sudo apt -y install \
   libreoffice-calc \
   libreoffice-draw \
   libreoffice-gnome \
-  libreoffice-gtk \
   libreoffice-gtk3 \
   libreoffice-math \
   libreoffice-nlpsolver \
   libreoffice-ogltrans \
-  libreoffice-pdfimport \
   libreoffice-writer \
   libreoffice-dmaths \
   libreoffice-lightproof-en \
--- install/dotnet.sh
+++ install/dotnet.sh
@@ -6,8 +6,9 @@ wget -q https://dot.net/v1/dotnet-install.sh -O $HOME/Tools/dotnet-install.sh
 chmod +x $HOME/Tools/dotnet-install.sh
 
 # sudo $HOME/Tools/dotnet-install.sh --channel 6.0 --install-dir /usr/share/dotnet
-sudo $HOME/Tools/dotnet-install.sh --channel 7.0 --install-dir /usr/share/dotnet
+# sudo $HOME/Tools/dotnet-install.sh --channel 7.0 --install-dir /usr/share/dotnet
 sudo $HOME/Tools/dotnet-install.sh --channel 8.0 --install-dir /usr/share/dotnet
+sudo $HOME/Tools/dotnet-install.sh --channel 9.0 --install-dir /usr/share/dotnet
 sudo ln -sf /usr/share/dotnet/dotnet /usr/bin/dotnet
 
 dotnet tool install --global dotnet-trace
